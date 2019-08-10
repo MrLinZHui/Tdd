2f48e83ffba2e66a1c1d67b0a34c3e5ddc3a3a7c
MrLinZHui <74284792@qq.com>
2019-08-10 13:29:46 +0800

    [MVP-1 #002]Tomi create test case should_return_list_win_when_give_a_double_tow__pair_list_and_a_tow_pair_list1

--- src/test/java/CardTest.java
+++ src/test/java/CardTest.java
@@ -66,4 +66,19 @@ public class CardTest {
         Assert.assertEquals("list1 win",result);
 
     }
+
+    @Test
+    public void should_return_list_win_when_give_a_double_tow__pair_list_and_a_tow_pair_list1(){
+        //given
+        List list = Arrays.asList(new Card("2s"),
+                new Card("2d"), new Card("3s"), new Card("3d"), new Card("7s"));
+        List list1 = Arrays.asList(new Card("4s"),
+                new Card("4d"), new Card("6s"), new Card("5d"), new Card("3s"));
+        HandleCard handleCard = new HandleCard();
+        //when
+        String result = HandleCard.compareCard(list, list1);
+        //then
+        Assert.assertEquals("list win",result);
+
+    }
 }
