fb7bcc204085daa5aaf98d63a857156c43e7dec1
MrLinZHui <74284792@qq.com>
2019-08-10 14:03:21 +0800

    [MVP-1 #002]Tomi create test case should_return_list1_win_when_give_two_three__pair_lists_and_list1_is_high_than_list

--- src/test/java/CardTest.java
+++ src/test/java/CardTest.java
@@ -131,17 +131,17 @@ public class CardTest {
         //then
         Assert.assertEquals("list win",result);
     }
-//    @Test
-//    public void should_return_list1_win_when_give_two_three__pair_lists_and_list1_is_high_than_list(){
-//        //given
-//        List list = Arrays.asList(new Card("2s"),
-//                new Card("2d"), new Card("2c"), new Card("6d"), new Card("7d"));
-//        List list1 = Arrays.asList(new Card("3s"),
-//                new Card("3d"), new Card("3c"), new Card("6d"), new Card("7s"));
-//        HandleCard handleCard = new HandleCard();
-//        //when
-//        String result = HandleCard.compareCard(list, list1);
-//        //then
-//        Assert.assertEquals("list1 win",result);
-//    }
+    @Test
+    public void should_return_list1_win_when_give_two_three__pair_lists_and_list1_is_high_than_list(){
+        //given
+        List list = Arrays.asList(new Card("2s"),
+                new Card("2d"), new Card("2c"), new Card("6d"), new Card("7d"));
+        List list1 = Arrays.asList(new Card("3s"),
+                new Card("3d"), new Card("3c"), new Card("6d"), new Card("7s"));
+        HandleCard handleCard = new HandleCard();
+        //when
+        String result = HandleCard.compareCard(list, list1);
+        //then
+        Assert.assertEquals("list1 win",result);
+    }
 }
