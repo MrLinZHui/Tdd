8f457359c07a2f61d971a7b5d5942f4f95b53a19
MrLinZHui <74284792@qq.com>
2019-08-10 12:47:31 +0800

    [MVP-1 #002]Tomi refactor the faction of compareCard

--- src/main/java/HandleCard.java
+++ src/main/java/HandleCard.java
@@ -6,38 +6,44 @@ import static java.util.Collections.sort;
 
 public class HandleCard {
     public static String compareCard(List<Card> list, List<Card> list1) {
-        List<Integer>numList = list.stream().map(Card::getNum).collect(Collectors.toList());
-        List<Integer>numList1 = list1.stream().map(Card::getNum).collect(Collectors.toList());
-        sort(numList);
-        sort(numList1);
+        List<Integer> numList = getSortIntegers(list);
+        List<Integer> numList1 = getSortIntegers(list1);
         int pairCount = 0;
         int pairCount1 = 0;
-        for (Integer num : numList) {
-            int count = Collections.frequency(numList, num);
-            if(count>1){
-               pairCount++;
-           }
-        }
-        for (Integer num : numList1) {
-            int count = Collections.frequency(numList1, num);
-            if(count>1){
-                pairCount1++;
-            }
-        }
-        System.out.println("count:"+pairCount+",count1:"+pairCount1);
+        pairCount = getPairCount(numList, pairCount);
+        pairCount1 = getPairCount(numList1, pairCount1);
         if(pairCount>pairCount1){
             return "list win";
         }
-        System.out.println(numList+","+numList1);
         if(numList.containsAll(numList1)){
             return "equal";
         }
-        int max1 = list.stream().mapToInt(Card::getNum).max().getAsInt();
-        int max2 = list1.stream().mapToInt(Card::getNum).max().getAsInt();
+        int max1 = getMax1(list);
+        int max2 = getMax1(list1);
         if(max1<max2){
             return "list1 win";
         }
         return null;
+    }
+
+    private static int getMax1(List<Card> list) {
+        return list.stream().mapToInt(Card::getNum).max().getAsInt();
+    }
+
+    private static List<Integer> getSortIntegers(List<Card> list) {
+        List<Integer> numList = list.stream().map(Card::getNum).collect(Collectors.toList());
+        sort(numList);
+        return numList;
+    }
+
+    private static int getPairCount(List<Integer> numList, int pairCount) {
+        for (Integer num : numList) {
+            int count = Collections.frequency(numList, num);
+            if (count > 1) {
+                pairCount++;
+            }
+        }
+        return pairCount;
     }
 
 }
